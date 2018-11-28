076271cb287bfaf1c68ba7daa15df42bee9db7c1
Maxim Alexandrov <3maxxy@gmail.com>
2018-11-28 21:11:25 +0300

    reformat code

--- src/test/java/ru/alexandrov/miniuniversity/view/controllers/PostControllerTest.java
+++ src/test/java/ru/alexandrov/miniuniversity/view/controllers/PostControllerTest.java
@@ -1,11 +1,9 @@
 package ru.alexandrov.miniuniversity.view.controllers;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
